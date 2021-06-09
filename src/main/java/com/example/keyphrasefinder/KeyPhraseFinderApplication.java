e9738b995912d534a307213bb17baa1ffc81a7e9
pashavel <pashavel27@gmail.com>
2021-06-09 13:55:29 +0300

    initial commit

---
+++ src/main/java/com/example/keyphrasefinder/KeyPhraseFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyphrasefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyPhraseFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyPhraseFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keyphrasefinder/KeyPhraseFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keyphrasefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyPhraseFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
